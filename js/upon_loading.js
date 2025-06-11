44b0269e3da91cd8be5555fb9ae22a119a2abdae
Filip <81096854+filipsam-my-github@users.noreply.github.com>
2025-06-11 13:21:15 +0200

    added datatype for checkboxes (view id)

--- js/upon_loading.js
+++ js/upon_loading.js
@@ -6,6 +6,18 @@ function setCheckboxesToChecked() {
   });
 }
 
+export function checkboxesDictionary() {
+  let checkboxes_dic = {};
+
+  let checkboxes = document.querySelectorAll("input[type='checkbox']");
+
+  checkboxes.forEach((checkbox) => {
+    checkboxes_dic[checkbox.id] = checkbox;
+  });
+
+  return checkboxes_dic;
+}
+
 export function setHTMLToDeft() {
   setCheckboxesToChecked();
 }
