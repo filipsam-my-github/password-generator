c20c16555780e4625b3208db8e0f9f8ca710d6f3
Filip <81096854+filipsam-my-github@users.noreply.github.com>
2025-06-11 13:17:36 +0200

    password generation file

---
+++ js/passwords.js
@@ -0,0 +1,47 @@
+import * as chars_types from "./characters.js";
+
+function getRandomItemFromArray(items) {
+  return items[Math.floor(Math.random() * items.length)];
+}
+
+export function generatePassword(
+  upercase_letters,
+  lowercase_letters,
+  numbers,
+  symbols,
+  length_of_password
+) {
+  let generating_password;
+  let avalableSymbols = [];
+
+  if (upercase_letters) {
+    avalableSymbols = [...avalableSymbols, ...chars_types.bigLetters];
+  }
+  if (lowercase_letters) {
+    avalableSymbols = [...avalableSymbols, ...chars_types.smallLetters];
+  }
+  if (numbers) {
+    avalableSymbols = [...avalableSymbols, ...chars_types.numbers];
+  }
+  if (symbols) {
+    avalableSymbols = [...avalableSymbols, ...chars_types.symbols];
+  }
+
+  if (avalableSymbols.length === 0) {
+    throw new Error(
+      `there was nothing selected password cannot be created from following symbols {exception rule avalableSymbols.length === 0} {avalableSymbols.length:${avalableSymbols.length},avalableSymbols:${avalableSymbols}}`
+    );
+  }
+
+  if (length_of_password <= 0) {
+    throw new Error(
+      `lenght of password prevents creating one {exception rule length_of_password <= 0} {length_of_password:${length_of_password}}`
+    );
+  }
+
+  for (let i = 0; i < length_of_password; i++) {
+    generating_password += getRandomItemFromArray(avalableSymbols);
+  }
+
+  return generating_password;
+}
